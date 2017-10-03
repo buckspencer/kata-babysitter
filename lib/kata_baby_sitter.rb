d3a6b89e3868aa2995c41eb408f668a48199749e
BuckSpencer <ad3uck@gmail.com>
2017-10-03 18:15:50 -0500

    add pay rates

--- lib/kata_baby_sitter.rb
+++ lib/kata_baby_sitter.rb
@@ -1,2 +1,7 @@
 module KataBabySitter
+
+  PRE_BED_RATE = 12
+  BED_TO_MIDNIGHT = 8
+  MIDNIGHT_TO_END = 16
+
 end
--- spec/kata_babysitter_spec.rb
+++ spec/kata_babysitter_spec.rb
@@ -2,8 +2,28 @@ require_relative '../lib/kata_baby_sitter'
 
 RSpec.describe "KataBabySitter" do
 
-  it 'should exist' do
-    expect(KataBabySitter.itself.name).to eql('KataBabySitter')
+  subject {KataBabySitter}
+
+  describe 'initial test' do 
+
+    it 'should exist' do
+      expect(subject.itself.name).to eql('KataBabySitter')
+    end
+
+  end
+
+  describe 'business logic' do 
+
+    context 'when rates are properly defined' do 
+
+      it { expect(subject::PRE_BED_RATE).to eql(12) }
+
+      it { expect(subject::BED_TO_MIDNIGHT).to eql(8) }
+
+      it { expect(subject::MIDNIGHT_TO_END).to eql(16) }
+
+    end
+
   end
 
 end
