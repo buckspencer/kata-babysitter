7f964943e1791d437df156222ab1699afb5e4e6d
BuckSpencer <ad3uck@gmail.com>
2017-10-03 20:16:35 -0500

    get method called in spec

--- lib/kata_baby_sitter.rb
+++ lib/kata_baby_sitter.rb
@@ -1,7 +1,16 @@
 module KataBabySitter
 
-  PRE_BED_RATE = 12
+  ## pay rates
+  PRE_BED = 12
   BED_TO_MIDNIGHT = 8
   MIDNIGHT_TO_END = 16
+
+  ## cut off range
+  HARD_START = 17
+  HARD_END = 4
+
+  def self.calc_payment start_time, end_time, bed_time
+    "hello"  
+  end
 
 end
--- spec/kata_babysitter_spec.rb
+++ spec/kata_babysitter_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "KataBabySitter" do
 
   subject {KataBabySitter}
 
-  describe 'initial test' do 
+  describe 'potential overkill test' do 
 
     it 'should exist' do
       expect(subject.itself.name).to eql('KataBabySitter')
@@ -14,16 +14,28 @@ RSpec.describe "KataBabySitter" do
 
   describe 'business logic' do 
 
-    context 'when rates are properly defined' do 
-
-      it { expect(subject::PRE_BED_RATE).to eql(12) }
+      it { expect(subject::PRE_BED).to eql(12) }
 
       it { expect(subject::BED_TO_MIDNIGHT).to eql(8) }
 
       it { expect(subject::MIDNIGHT_TO_END).to eql(16) }
 
-    end
+      it { expect(subject::HARD_START).to eql(17) }
 
+      it { expect(subject::HARD_END).to eql(4) }
+
+  end
+
+
+  describe 'calc_payment' do 
+    let(:start_time) { '6pm' }
+    let(:end_time)   { '3am'}
+    let(:bed_time)   {'9pm'}
+
+    subject { KataBabySitter.calc_payment(start_time, end_time, bed_time) }
+
+    # it { expect(subject).to eql(108) }
+    it { expect(subject).to eql('hello')}
   end
 
 end
