8150c40413e2806b4095d2d585bef8fa0407a52a
BuckSpencer <ad3uck@gmail.com>
2017-10-04 20:36:43 -0500

    one last test

--- spec/kata_babysitter_spec.rb
+++ spec/kata_babysitter_spec.rb
@@ -20,11 +20,31 @@ RSpec.describe "KataBabySitter" do
 
       it { expect(KataBabySitter::HARD_END.hour).to eql(4) }
 
+    context 'error handling' do 
+      context 'when arguments are correctly given' do 
+        it { expect{subject}.not_to raise_error }
+      end
+
+
+      context 'when start_time is later than end_time' do 
+        let(:end_time)   { '6am'}
+
+        it { expect{subject}.to raise_error(ArgumentError, "Start and end times must be within given range.") }
+      end
+
+      context 'when end_time is before than start_time' do 
+        let(:end_time)   { '5pm'}
+
+        it { expect{subject}.to raise_error(ArgumentError, "Start time must come before end time.") }
+      end
+    end
+
   end
 
 
+
+
   describe 'calc_payment' do 
-    
 
     before {subject.calc_payment}
 
@@ -33,71 +53,45 @@ RSpec.describe "KataBabySitter" do
       it { is_expected.to eql(108)}
     end
 
-
-    context 'error handling' do 
-      context 'when arguments are correctly given' do 
-        it { is_expected.not_to raise_error }
-      end
-
-
-      context 'when start_time is later than end_time' do 
-        let(:end_time)   { '6am'}
-
-        it { is_expected.to raise_error(ArgumentError, "Start and end times must be within given range.") }
-      end
-
-      context 'when end_time is before than start_time' do 
-        let(:end_time)   { '5pm'}
-
-        it { is_expected.to raise_error(ArgumentError, "Start time must come before end time.") }
-      end
-    end
   end
 
 
   describe 'calc_pre_bed' do 
 
     context 'when proper arguments' do
-      it { expect{subject.calc_pre_bed}.to eql(36)}
+      it { expect(subject.calc_pre_bed).to eql(36)}
     end
-
 
   end
 
   describe 'calc_bed_to_midnight' do 
 
-    subject { KataBabySitter.new.calc_bed_to_midnight(bed_time)}
-
     context 'when proper arguments' do 
-      it { expect(subject).to eql(24)}
+      it { expect(subject.calc_bed_to_midnight).to eql(24)}
     end
 
   end
 
   describe 'calc_midnight_to_end' do 
 
-    subject { KataBabySitter.new.calc_midnight_to_end(end_time)}
-
     context 'when proper arguments' do 
-      it { expect(subject).to eql(48)}
+      it { expect(subject.calc_midnight_to_end).to eql(48)}
     end
 
   end
 
   describe 'format_time' do 
 
-    subject { KataBabySitter.new.format_time(time)}
-
     context 'when contains am' do 
       let(:time) { '3am' }
 
-      it { expect(subject).to eql(Time.parse(time) + 86400) }
+      it { expect(subject.format_time(time)).to eql(Time.parse(time) + 86400) }
     end
 
     context 'when contains pm' do 
       let(:time) { '3pm' }
 
-      it { expect(subject).to eql(Time.parse(time)) }
+      it { expect(subject.format_time(time)).to eql(Time.parse(time)) }
     end
   end
 
