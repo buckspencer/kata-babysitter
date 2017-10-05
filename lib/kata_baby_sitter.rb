3ef2a5eaf1908f17c3eac2266bb5d57cb6ebaca2
BuckSpencer <ad3uck@gmail.com>
2017-10-04 21:10:04 -0500

    try to break code

--- lib/kata_baby_sitter.rb
+++ lib/kata_baby_sitter.rb
@@ -2,22 +2,22 @@ require 'time'
 
 class KataBabySitter
 
+  DAY_IN_SECS = 86_400
   MIDNIGHT = 24
   HARD_START = Time.parse('5pm')
-  HARD_END = Time.parse('4am') + 86400
+  HARD_END = Time.parse('4am') + DAY_IN_SECS
 
   RATES = [PRE_BED = 12, BED_TO_MIDNIGHT = 8, MIDNIGHT_TO_END = 16]
 
-  attr_reader :start_time, :end_time, :bed_time 
-
+  attr_reader :start_time, :end_time, :bed_time
 
   def initialize start_t, end_t, bed_t
     @start_time = format_time(start_t)
     @end_time = format_time(end_t)
     @bed_time = format_time(bed_t)
 
-    time_range_error if requirements_not_met?
-    order_error if start_time > end_time
+    raise time_range_error if requirements_not_met?
+    raise order_error if start_time > end_time
   end
 
   def calc_payment
@@ -37,21 +37,19 @@ class KataBabySitter
   end
 
   def format_time time
-    time.include?('am') ? Time.parse(time) + 86400 : Time.parse(time)
+    time.include?('am') ? Time.parse(time) + DAY_IN_SECS : Time.parse(time)
   end
 
   def requirements_not_met?
     (start_time <= HARD_START || end_time >= HARD_END)
   end
 
-
   def time_range_error
-    raise ArgumentError.new("Start and end times must be within given range.")
+    ArgumentError.new("Start and end times must be within given range.")
   end
 
-
   def order_error
-    raise ArgumentError.new("Start time must come before end time.")
+    ArgumentError.new("Start time must come before end time.")
   end
 
 end
--- spec/kata_babysitter_spec.rb
+++ spec/kata_babysitter_spec.rb
@@ -1,91 +1,104 @@
 require 'pry'
 require_relative '../lib/kata_baby_sitter'
 
-RSpec.describe "KataBabySitter" do
-  let(:start_time) {'6pm'}
-  let(:end_time)   {'3am'}
-  let(:bed_time)   {'9pm'}
+RSpec.describe 'KataBabySitter' do
+  let(:start_time) { '6pm' }
+  let(:end_time)   { '3am' }
+  let(:bed_time)   { '9pm' }
 
   subject { KataBabySitter.new(start_time, end_time, bed_time) }
 
-  describe 'business logic' do 
+  describe 'business logic' do
 
-      it { expect(KataBabySitter::PRE_BED).to eql(12) } #36
+      it { expect(KataBabySitter::DAY_IN_SECS).to eql(86_400)}
 
-      it { expect(KataBabySitter::BED_TO_MIDNIGHT).to eql(8) } #24
+      it { expect(KataBabySitter::MIDNIGHT).to eql(24)}  
 
-      it { expect(KataBabySitter::MIDNIGHT_TO_END).to eql(16) } #48 
+      it { expect(KataBabySitter::PRE_BED).to eql(12) }
+
+      it { expect(KataBabySitter::BED_TO_MIDNIGHT).to eql(8) }
+
+      it { expect(KataBabySitter::MIDNIGHT_TO_END).to eql(16) } 
 
       it { expect(KataBabySitter::HARD_START.hour).to eql(17) }
 
       it { expect(KataBabySitter::HARD_END.hour).to eql(4) }
 
     context 'error handling' do 
-      context 'when arguments are correctly given' do 
-        it { expect{subject}.not_to raise_error }
+      context 'when arguments are correctly given' do
+        it { expect{ subject }.not_to raise_error }
+      end
+
+      context 'when nils are passed' do 
+        let(:start_time) { nil }
+        let(:end_time)   { nil }
+        let(:bed_time)   { nil }
+
+        it { expect{ subject }.to raise_error(ArgumentError, "You must add valid arguments.") }
       end
 
 
-      context 'when start_time is later than end_time' do 
-        let(:end_time)   { '6am'}
+      context 'when start_time is later than end_time' do
+        let(:end_time)   { '6am' }
 
-        it { expect{subject}.to raise_error(ArgumentError, "Start and end times must be within given range.") }
+        it { expect{ subject }.to raise_error(ArgumentError, "Start and end times must be within given range.") }
       end
 
       context 'when end_time is before than start_time' do 
-        let(:end_time)   { '5pm'}
+        let(:end_time)   { '5pm' }
 
-        it { expect{subject}.to raise_error(ArgumentError, "Start time must come before end time.") }
+        it { expect{ subject }.to raise_error(ArgumentError, "Start time must come before end time.") }
       end
     end
 
   end
-
 
 
 
   describe 'calc_payment' do 
 
-    context 'working correctly' do 
-      it { expect(subject.calc_payment).to eql(108)}
+    context 'working correctly' do
+      it { expect(subject.calc_payment).to eql(108) }
     end
+
+
 
   end
 
 
-  describe 'calc_pre_bed' do 
+  describe 'calc_pre_bed' do
 
     context 'when proper arguments' do
-      it { expect(subject.calc_pre_bed).to eql(36)}
+      it { expect(subject.calc_pre_bed).to eql(36) }
     end
 
   end
 
-  describe 'calc_bed_to_midnight' do 
+  describe 'calc_bed_to_midnight' do
 
     context 'when proper arguments' do 
-      it { expect(subject.calc_bed_to_midnight).to eql(24)}
+      it { expect(subject.calc_bed_to_midnight).to eql(24) }
     end
 
   end
 
-  describe 'calc_midnight_to_end' do 
+  describe 'calc_midnight_to_end' do
 
     context 'when proper arguments' do 
-      it { expect(subject.calc_midnight_to_end).to eql(48)}
+      it { expect(subject.calc_midnight_to_end).to eql(48) }
     end
 
   end
 
-  describe 'format_time' do 
+  describe 'format_time' do
 
     context 'when contains am' do 
       let(:time) { '3am' }
 
-      it { expect(subject.format_time(time)).to eql(Time.parse(time) + 86400) }
+      it { expect(subject.format_time(time)).to eql(Time.parse(time) + KataBabySitter::DAY_IN_SECS) }
     end
 
-    context 'when contains pm' do 
+    context 'when contains pm' do
       let(:time) { '3pm' }
 
       it { expect(subject.format_time(time)).to eql(Time.parse(time)) }
