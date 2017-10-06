b631ccd38730c1e2db91d8d28c81914d15b0b0b0
BuckSpencer <ad3uck@gmail.com>
2017-10-05 23:40:22 -0500

    same tests, more progress, getting late, on a roll

--- lib/kata_baby_sitter.rb
+++ lib/kata_baby_sitter.rb
@@ -2,10 +2,9 @@ require 'time'
 
 class KataBabySitter
 
-  DAY_IN_SECS = 86_400
-  MIDNIGHT = 24
-  HARD_START = Time.parse('5pm') 
-  HARD_END = Time.parse('4am') + DAY_IN_SECS
+  MIDNIGHT = 12
+  HARD_START = 5 
+  HARD_END = 16
 
   RATES = [PRE_BED = 12, BED_TO_MIDNIGHT = 8, MIDNIGHT_TO_END = 16]
 
@@ -30,8 +29,28 @@ class KataBabySitter
     [calc_pre_bed, calc_bed_to_midnight, calc_midnight_to_end].sum
   end
 
+  def calc_pre_bed
+    (bed_time - start_time) * PRE_BED
+  end
+
+  def calc_bed_to_midnight
+    (end_time > MIDNIGHT ? MIDNIGHT - bed_time : end_time - bed_time) * BED_TO_MIDNIGHT
+  end
+
+  def calc_midnight_to_end 
+    end_time > MIDNIGHT ? (end_time - MIDNIGHT) * MIDNIGHT_TO_END : 0
+  end
+
   def format_time time 
-    time < 5 ? time + 10 : time
+    time < 5 ? time + 12 : time
+  end
+
+  def inputs_improperly_formatted? start_t, end_t, bed_t
+    [start_t, end_t, bed_t].join.include?('.')
+  end
+
+  def out_of_sequence?
+    (start_time < HARD_START || end_time > HARD_END) || (bed_time > end_time) || (start_time > end_time)
   end
 
   def run_time_error
